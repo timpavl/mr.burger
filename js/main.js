779b00de08c239fba28aad8e5c0ec41da686458e
timomsk <tim160594@yandex.ru>
2019-03-09 21:58:52 +0600

    js

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@ leftArrow.addEventListener('click', () => {
 });
 
 /*--- TEAM ---*/
-
+/*
 // инициализация констант
 const teamItems = document.querySelectorAll('.team__li');
 const teamBtns = document.querySelectorAll('.team__li-button');
@@ -48,7 +48,7 @@ const teamBtns = document.querySelectorAll('.team__li-button');
 // добавить кнопку слушателя для кнопки
 Array.from(teamBtns).forEach(btn => {
   btn.addEventListener('click', e => {
-    if (e.target.parentElement.classList.contains("team__li_active")) {
+    if (e.target.parentElement.classList.contains("team__li_align-center")) {
       closeTeamContent();
     } else {
       closeTeamContent();
@@ -60,7 +60,7 @@ Array.from(teamBtns).forEach(btn => {
 // функция открытия содержимого команды
 function openTeamContent(e) {
   const item = e.target.parentElement;
-  item.classList.add("team__li_active");
+  item.classList.add("team__li_align-center");
 
   const contentWrapper = e.target.nextElementSibling;
   const content = contentWrapper.querySelector('.team__li-content');
@@ -72,8 +72,46 @@ function openTeamContent(e) {
 // функция закрытия содержимого команды
 function closeTeamContent() {
   Array.from(teamItems).forEach(item => {
-    item.classList.remove("team__li_active");
+    item.classList.remove("team__li_align-center");
     const contentWrapper = item.querySelector(".team__li-content");
     contentWrapper.style.height = 0;
   });
 }
+*/
+$(document).ready(function () { 
+
+  $('.team__li-button').on('click', function (a) {
+    a.preventDefault(); //Отмена действия браузера
+
+    var elem = $(a.target),
+      item = elem.closest('.team__li'), //Строка содержащая выражение селектора для проверки совпадения элементов.
+      items = item.siblings(); //Строка, содержащая выражение селектора для сопоставления элементов.
+      /*Описание siblings: Осуществляет поиск элементов, являющихся соседними для выбранных элементов (под соседними понимаются элементы, которые имеют общего родителя). При этом, сами выбранные элементы в результат не включаются.*/
+    if (!item.hasClass('team-active')) { //присвоение класса 
+      items.removeClass('team-active');
+      item.addClass('team-active');
+    } else {
+      item.removeClass('team-active');
+    }
+  });
+
+});
+//Меню-Аккордеон 
+$(document).ready(function () {
+
+  $('.menu__tint').on('click', function (a) {
+    a.preventDefault();
+
+    var elem = $(a.target),
+      item = elem.closest('.menu__li'),
+      items = item.siblings();
+
+    if (!item.hasClass('active')) {
+      items.removeClass('active');
+      item.addClass('active');
+    } else {
+      item.removeClass('active');
+    }
+  });
+
+});
